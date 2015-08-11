4a35a87786dc020a1ee0374d054e0290b2ba7fdb
wjonesy <wtrjones@gmail.com>
2015-08-11 23:47:40 +0100

    Added Gulp to compile LESS

---
+++ Sarah.Henderson/gulpfile.js
@@ -0,0 +1,18 @@
+﻿/// <binding BeforeBuild='less' />
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var plumber = require('gulp-plumber');
+var watch = require('gulp-watch');
+gulp.task('less', function () {
+    return gulp.src('./css/main.less')
+    .pipe(plumber())
+      .pipe(less({
+          paths: [path.join(__dirname, 'less', 'includes')]          
+      }))
+        
+      .pipe(gulp.dest('./css/'));
+});
+gulp.task('watch', function() {
+    gulp.watch('./css/*.less', ['less']);  // Watch all the .less files, then run the less task
+});
